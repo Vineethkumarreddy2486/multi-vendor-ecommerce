007e4dea1e88396b1fb3c82bc03e35b012e8fe5d
Vineethkumarreddy2486 <vineethkumarreddy2486@gmail.com>
2026-05-20 23:37:27 +0530

    fixed deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,15 @@
 import axios from 'axios';
 
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api',
+//   headers: { 'Content-Type': 'application/json' }
+// });
+
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: { 'Content-Type': 'application/json' }
+  baseURL:
+    window.location.hostname === 'localhost'
+      ? 'http://localhost:5000/api'
+      : 'https://YOUR-RENDER-URL.onrender.com/api'
 });
 
 API.interceptors.request.use((config) => {
